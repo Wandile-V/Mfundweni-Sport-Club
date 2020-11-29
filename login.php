fa06fbdbfa4ca49cf8b53c1a18e53ea845fd96c3
Wandile-V <50173602+Wandile-V@users.noreply.github.com>
2020-11-30 01:45:01 +0200

    create login.php page

---
+++ login.php
@@ -0,0 +1,30 @@
+!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+<meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
+<title>Login</title>
+<link  rel="stylesheet" type="text/css" href="css/style.css"/>
+</head>
+
+<body>
+	<img src="images/MSCLogo.jpg" alt="MSC Logo" class="logo"/>
+	<div id="navbar">		
+		<ul class="nav navbar-nav"> 
+			<li><a href="index.html">Home</a></li>
+			<li><a href="about-us.html">About Us</a></li>
+			<li><a href="teams.html">Teams</a></li>
+			<li><a href="gallery.html">Gallery</a></li>						
+			<li><a href="contact-us.html">Contact Us</a></li>						
+		</ul>
+	</div>
+	<div id="container">
+		<h1>Login</h1>
+        
+        
+        
+	</div>
+
+</body>
+
+</html>
