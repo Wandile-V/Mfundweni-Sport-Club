79c853b8cf72881383545ab8be7df2af8e1ec883
Wandile-V <50173602+Wandile-V@users.noreply.github.com>
2020-12-01 12:27:23 +0200

    rename page from html to php

---
+++ contact-us.php
@@ -0,0 +1,35 @@
+﻿<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+<meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
+<link rel="stylesheet" type="text/css" href="css/style.css"/>
+<title>Contact Us</title>
+</head>
+	<img src="images/MSCLogo.jpg" class="logo"/>
+	<body>
+		<div id="navbar" class="navbar-collapse collapse">
+		<ul class="nav navbar-nav">
+			<li><a href="index.html">Home</a></li>
+			<li><a href="about-us.html">About Us</a></li>
+			<li><a href="teams.html">Teams</a></li>
+			<li><a href="gallery.html">Gallery</a></li>						
+			<li><a href="contact-us.html">Contact Us</a></li>						
+		</ul>
+	</div>
+
+	<div id="container">
+	<h3>Please feel free to contact us on the following information</h3>
+
+	Email: scemfundweni@gmail.com<br><br>
+	Facebook: Mfundweni SC<br><br>
+
+	Address:<br> 
+	Mfundweni location<br>
+	Umzimkhulu<br>
+	3297	
+	</div>
+
+</body>
+
+</html>
