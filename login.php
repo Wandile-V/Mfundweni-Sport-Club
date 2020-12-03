d6592c09ffffce132edb53e873734d5ea33aeafd
Wandile-V <50173602+Wandile-V@users.noreply.github.com>
2020-12-03 12:40:15 +0200

    add changes

--- login.php
+++ login.php
@@ -1,8 +1,9 @@
-!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html >
+<html>
 
 <head>
-<meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
 <title>Login</title>
 <link  rel="stylesheet" type="text/css" href="css/style.css"/>
 </head>
