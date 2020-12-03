a8ba55fc96e5e632b854e6b79d23f3d6e736ee14
Wandile-V <50173602+Wandile-V@users.noreply.github.com>
2020-12-03 16:46:57 +0200

    create member and player tables

---
+++ database/mfundweni-sport-club.sql
@@ -0,0 +1,14 @@
+CREATE TABLE Members{
+    memberName VARCHAR(20) NOT NULL,
+    surname VARCHAR(20) NOT NULL,
+    email VARCHAR(20) NOT NULL,
+    phone VARCHAR(10) NOT NULL,
+    memberPassword VARCHAR(30) NOT NULL
+}
+
+CREATE TABLE Players{
+    playerName VARCHAR(20) NOT NULL,
+    surname VARCHAR(20) NOT NULL,
+    email VARCHAR(20) NULL,
+    dateOfBirth DATE NOT NULL
+}
