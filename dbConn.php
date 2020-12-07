9d9df6c6fe5f2a272edc12c788be131d05be8e3c
Wandile-V <50173602+Wandile-V@users.noreply.github.com>
2020-12-07 21:42:25 +0200

    create connection if else statements

--- dbConn.php
+++ dbConn.php
@@ -1,8 +1,20 @@
 <?php
 //connects to the database
-$db_host
-$db_username
-$db_password
-$db_name
+$db_host = "";
+$db_username = "";
+$db_password = "";
+$db_name = "";
+
+$db_connect = mysqli_connect($db_host, $db_username, $db_password, $db_name);
+
+    //check connection
+    if(mysqli_connection_error())
+    {
+        echo "<h3>Failed to connect to mySQL: </h3>" .mysqli_connect_error()
+    }
+    else
+    {
+        echo "<h3>Connected!</h3>"; //When connected 
+    }
 
 ?>
