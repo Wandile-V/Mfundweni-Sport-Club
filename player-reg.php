51f278a7dd0afa7d9e8fef340ec8ecc2c0c0320a
Wandile-V <50173602+Wandile-V@users.noreply.github.com>
2021-01-28 11:46:13 +0200

    change page titles and add comments

---
+++ player-reg.php
@@ -0,0 +1,30 @@
+!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+<meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
+<title>Player Registration</title>
+<link  rel="stylesheet" type="text/css" href="css/style.css"/>
+</head>
+
+<body>
+	<img src="images/MSCLogo.jpg" alt="MSC Logo" class="logo"/>
+	<div id="navbar">		
+		<ul class="nav navbar-nav"> 
+			<li><a href="index.php">Home</a></li>
+			<li><a href="about-us.php">About Us</a></li>
+			<li><a href="teams.php">Teams</a></li>
+			<li><a href="gallery.php">Gallery</a></li>						
+			<li><a href="contact-us.php">Contact Us</a></li>						
+		</ul>
+	</div>
+	<div id="container">
+		 <h1>Player registration</h1> <!-- Player registration section -->
+        
+    
+        
+	</div>
+
+</body>
+
+</html>
