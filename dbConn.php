538c69c3686c33805a6ab9ae4014b47bb792ce19
Wandile-V <50173602+Wandile-V@users.noreply.github.com>
2021-03-16 21:29:11 +0200

    fix syntax errors

--- dbConn.php
+++ dbConn.php
@@ -10,7 +10,7 @@ $db_connect = mysqli_connect($db_host, $db_username, $db_password, $db_name);
     //check connection
     if(mysqli_connection_error())
     {
-        echo "<h3>Failed to connect to mySQL: </h3>" .mysqli_connect_error()
+        echo "<h3>Failed to connect to mySQL: </h3>".mysqli_connect_error();
     }
     else
     {
