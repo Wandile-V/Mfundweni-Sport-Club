8653db3d26e199774cfeb1c799ccc66ab5dbf6a8
Keoabetswe <keo.nthite@iress.com>
2021-06-28 13:55:14 +0200

    add member for post values

--- server.php
+++ server.php
@@ -2,13 +2,13 @@
 //************************** Register Member **********************//
 if(isset($_POST['register-submit']))
 {
-    $name = mysqli_real_escape_string($db_connect, $_POST['name']);
-    $surname = mysqli_real_escape_string($db_connect, $_POST['surname']);
-    $email = mysqli_real_escape_string($db_connect, $_POST['email']);
-    $phone = mysqli_real_escape_string($db_connect, $_POST['phone']);
-    $password = mysqli_real_escape_string($db_connect, $_POST['password']);
+    $name = mysqli_real_escape_string($db_connect, $_POST['member-name']);
+    $surname = mysqli_real_escape_string($db_connect, $_POST['member-surname']);
+    $email = mysqli_real_escape_string($db_connect, $_POST['member-email']);
+    $phone = mysqli_real_escape_string($db_connect, $_POST['member-phone']);
+    $password = mysqli_real_escape_string($db_connect, $_POST['member-password']);
     //******************insert form values */
-    $registerQuery = "INSERT INTO members (memberName, memberSurname, memberEmail, memberPhone, memberPassword) VALUES ('$name, $surname, $phone, $password')";
+    $registerQuery = "INSERT INTO members (memberName, memberSurname, memberEmail, memberPhone, memberPassword) VALUES ('$name, $surname, $email, $phone, $password')";
     mysqli_query($db_connect, $registerQuery);
 
     header('location: login.php');
@@ -33,7 +33,7 @@ if(isset($_POST['login-submit']))
 {
     //Read all values inserted in the login form
     $memberEmail = mysqli_real_escape_string($db_connect, $_POST['member-email']);
-    $memberPassword = mysqli_real_escape_string($db_connect, $_POST['password']);
+    $memberPassword = mysqli_real_escape_string($db_connect, $_POST['member-password']);
 
     //Read from the database
     $loginQuery = "SELECT * FROM members WHERE 'memberEmail = $memberEmail' AND 'memberPassword = $memberPassword'";
