f845fd118f12ed1b7ace869942f3af4ca2bc560b
Wandile-V <50173602+Wandile-V@users.noreply.github.com>
2021-08-12 22:30:50 +0200

    insert db_password

--- dbConn.php
+++ dbConn.php
@@ -2,7 +2,7 @@
 //connects to the database
 $db_host = "us-cdbr-east-04.cleardb.com";
 $db_username = "b42deaba54b945";
-$db_password = "";
+$db_password = "c1f907bf";
 $db_name = "";
 
 $db_connect = mysqli_connect($db_host, $db_username, $db_password, $db_name);
