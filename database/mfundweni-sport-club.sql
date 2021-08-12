453a3d7af72bd61ad5e2ac881b82991395f20221
Wandile-V <50173602+Wandile-V@users.noreply.github.com>
2021-08-12 23:09:45 +0200

    change curly braces into brackets

--- database/mfundweni-sport-club.sql
+++ database/mfundweni-sport-club.sql
@@ -1,26 +1,26 @@
-CREATE TABLE members{
+CREATE TABLE members(
     memberID INT(10) PRIMARY KEY NOT NULL AUTO_INCREMENT,
     memberName VARCHAR(20) NOT NULL,
     memberSurname VARCHAR(20) NOT NULL,
     memberEmail VARCHAR(20) NOT NULL,
     memberPhone VARCHAR(10) NOT NULL,
     memberPassword VARCHAR(30) NOT NULL
-}
+)
 
-CREATE TABLE players{
+CREATE TABLE players(
     playerID INT(10) PRIMARY KEY NOT NULL AUTO_INCREMENT, 
     playerName VARCHAR(20) NOT NULL,
     playerSurname VARCHAR(20) NOT NULL,
     playerEmail VARCHAR(20) NULL,
     dateOfBirth DATE NOT NULL,
     position VARCHAR(30) NOT NULL
-}
+)
 
-CREATE TABLE playerRegistration{
+CREATE TABLE playerRegistration(
     playerName VARCHAR(20) NOT NULL,
     playerSurname VARCHAR(20) NOT NULL,
     position VARCHAR(20) NOT NULL,
     phone VARCHAR(10) NOT NULL
-}
+)
 
 INSERT INTO players VALUES (1, "King", "Veza", "King@gmail.com", DATE, "Midfielder"); /*Line to test inserts*/
