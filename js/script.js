2b64718de696bd7882369f9662c315e86575bb6d
Wandile-V <50173602+Wandile-V@users.noreply.github.com>
2021-09-22 11:14:49 +0200

    toggle nav-active class

--- js/script.js
+++ js/script.js
@@ -1,4 +1,8 @@
 const navSlide = () => {
     const burger = document.querySelector('.burger');
     const nav = document.querySelector('.navbar');
+
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('nav-active')
+    })
 }
