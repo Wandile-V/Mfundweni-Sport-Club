374cd2247ff12009e1f34a012f7fe599fb28758b
Keoabetswe <keo.nthite@iress.com>
2021-09-26 10:15:57 +0200

    add burger class

--- index.php
+++ index.php
@@ -7,30 +7,30 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<link href="css/style.css" type="text/css" rel="stylesheet"/>
 	</head>
-	<body>
-	<nav>
-		<div class="logo">
-			<a href="index.php" ><img src="images/MSCLogo.jpg" alt="MSC logo" class="logo"/></a>
-		</div>
-		<ul class="nav-links">
-			<li><a href="index.php">Home</a></li>
-			<li><a href="about-us.php">About Us</a></li>
-			<li><a href="teams.php">Teams</a></li>
-			<li><a href="gallery.html">Gallery</a></li>						
-			<li><a href="contact-us.php">Contact Us</a></li>
-			<li><a href="login.php"><button class="navLoginButton">Login</button></a></li>						
-		</ul>
-		<div class="burger">
-			<div class="line1"></div>
-			<div class="line2"></div>
-			<div class="line3"></div>
-		</div>
-	</nav>
-	<script src="js/jscript.js"></script>
+<body>
+<nav>
+	<div class="logo">
+		<a href="#"><img src="images/MSCLogo.jpg" alt="MSC Logo" class="logoImg"/></a>	
+	</div>
+	<ul class="navbar"> 	
+		<li><a href="index.php">Home</a></li>
+		<li><a href="about-us.php">About Us</a></li>
+		<li><a href="teams.php">Teams</a></li>
+		<li><a href="gallery.html">Gallery</a></li>						
+		<li><a href="contact-us.php">Contact Us</a></li>	
+		<li><a href="login.php"><button class="navLoginButton">Login</button></a></li>
+	</ul>
+	<div class="burger">
+		<div class="line1"></div>
+		<div class="line2"></div>
+		<div class="line3"></div>
+	</div>
+</nav>
 	
-	
-		<div class="container">
-			<h1>Welcome to the Mfundweni Sport Club page</h1>
-		</div>
-	</body>
+
+	<div class="container">
+		<h1>Mfundweni Sporting Club</h1>
+	</div>
+<script src="js/script.js"></script>
+</body>
 </html>
