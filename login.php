958d17e58fc78b44f54505031ba3994dbd15e62f
Wandile-V <50173602+Wandile-V@users.noreply.github.com>
2021-09-27 22:32:38 +0200

    create navbar with hamburger menu

--- login.php
+++ login.php
@@ -9,18 +9,24 @@
 
 <body>
 <nav>
-	
-</nav>
-	<div class="navbar">		
-		<ul class="nav navbar-nav"> 
-			<li><a href="index.php"><img src="images/MSCLogo.jpg" alt="MSC Logo" class="logo"/></a></li>
-			<li><a href="index.php">Home</a></li>
-			<li><a href="about-us.php">About Us</a></li>
-			<li><a href="teams.php">Teams</a></li>
-			<li><a href="gallery.html">Gallery</a></li>						
-			<li><a href="contact-us.php">Contact Us</a></li>						
-		</ul>
+	<div class="logo">
+		<a href="#"><img src="images/MSCLogo.jpg" alt="MSC Logo" class="logoImg"/></a>	
 	</div>
+	<ul class="navbar"> 	
+		<li><a href="index.php">Home</a></li>
+		<li><a href="about-us.php">About Us</a></li>
+		<li><a href="teams.php">Teams</a></li>
+		<li><a href="gallery.html">Gallery</a></li>						
+		<li><a href="contact-us.php">Contact Us</a></li>	
+		<li><a href="login.php"><button class="navLoginButton">Login</button></a></li>
+	</ul>
+	<div class="burger">
+		<div class="line1"></div>
+		<div class="line2"></div>
+		<div class="line3"></div>
+	</div>
+</nav>
+
 	<div class="form">
         <form class="contact-form" action="server.php" method="POST">
 			<h1>Login</h1>
