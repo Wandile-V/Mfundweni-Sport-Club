8eed285897205d4b1ec9e169b9d93618380e6c07
Wandile-V <50173602+Wandile-V@users.noreply.github.com>
2021-09-27 22:39:41 +0200

    insert navbar with hamburger menu

--- contact-us.php
+++ contact-us.php
@@ -6,16 +6,24 @@
 	<title>Contact Us</title>
 </head>
 <body>
-	<div class="navbar">
-		<ul class="nav navbar-nav">
-			<li><a href="index.php"><img src="images/MSCLogo.jpg" alt="MSC Logo" class="logo"/></a></li>
-			<li><a href="index.php">Home</a></li>
-			<li><a href="about-us.php">About Us</a></li>
-			<li><a href="teams.php">Teams</a></li>
-			<li><a href="gallery.html">Gallery</a></li>						
-			<li><a href="contact-us.php">Contact Us</a></li>						
-		</ul>
+<nav>
+	<div class="logo">
+		<a href="#"><img src="images/MSCLogo.jpg" alt="MSC Logo" class="logoImg"/></a>	
 	</div>
+	<ul class="navbar"> 	
+		<li><a href="index.php">Home</a></li>
+		<li><a href="about-us.php">About Us</a></li>
+		<li><a href="teams.php">Teams</a></li>
+		<li><a href="gallery.html">Gallery</a></li>						
+		<li><a href="contact-us.php">Contact Us</a></li>	
+		<li><a href="login.php"><button class="navLoginButton">Login</button></a></li>
+	</ul>
+	<div class="burger">
+		<div class="line1"></div>
+		<div class="line2"></div>
+		<div class="line3"></div>
+	</div>
+</nav>
 
 	<div class="container">
 	<h3>Please feel free to contact us on the following information</h3>
