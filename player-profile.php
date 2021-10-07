ba1f602a82db61b24311907844dc9ce5417f4dfc
Wandile-V <50173602+Wandile-V@users.noreply.github.com>
2021-10-07 23:28:03 +0200

    insert nav with hamburger menu

--- player-profile.php
+++ player-profile.php
@@ -22,9 +22,27 @@
         <link href="css/style.css" type="text/css" rel="stylesheet">
     </head>
     <body>
+	<nav>
+	<div class="logo">
+		<a href="index.php"><img src="images/MSCLogo.jpg" alt="MSC Logo" class="logoImg"/></a>	
+	</div>
+	<ul class="navbar"> 	
+		<li><a href="index.php">Home</a></li>
+		<li><a href="about-us.php">About Us</a></li>
+		<li><a href="teams.php">Teams</a></li>
+		<li><a href="gallery.html">Gallery</a></li>						
+		<li><a href="contact-us.php">Contact Us</a></li>	
+		<li><a href="login.php"><button class="navLoginButton">Login</button></a></li>
+	</ul>
+	<div class="burger">
+		<div class="line1"></div>
+		<div class="line2"></div>
+		<div class="line3"></div>
+	</div>
+</nav>
         
         <div class="container">
-            <p id="player-profile">Hello World</p>    
+        <!--     <p id="player-profile">Hello World</p>    
              <div id="navbar" class="navbar-collapse collapse">
                 <ul class="nav navbar-nav">
 					<li><a href="index.php"><img src="images/MSCLogo.jpg" alt="MSC Logo" class="logo"/></a></li>
@@ -34,7 +52,7 @@
                     <li><a href="gallery.html">Gallery</a></li>						
                     <li><a href="contact-us.php">Contact Us</a></li>
                 </ul>
-            </div>
+            </div> -->
         
             <div class="player profile">
                
