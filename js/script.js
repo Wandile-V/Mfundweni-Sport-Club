62b2fa36750020889caed8c28abac17e691486c1
Wandile-V <50173602+Wandile-V@users.noreply.github.com>
2021-10-10 19:08:43 +0200

    add navSlide()

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
 const navSlide = () => {
     const burger = document.querySelector('.burger');
     const nav = document.querySelector('.navbar');
-   
 
     burger.addEventListener('click', () => {
         nav.classList.toggle('nav-active');
