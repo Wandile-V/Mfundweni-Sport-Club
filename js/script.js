00b77aaa2c573e4892149851630469ff6f725e68
Wandile-V <50173602+Wandile-V@users.noreply.github.com>
2021-10-13 21:36:18 +0200

    add querySelectorAll for navbar li

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 const navSlide = () => {
     const burger = document.querySelector('.burger');
     const nav = document.querySelector('.navbar');
+    const navBar = document.querySelectorAll('.navbar li')
 
     burger.addEventListener('click', () => {
         nav.classList.toggle('nav-active');
