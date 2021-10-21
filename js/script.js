fa3e9e206ec1b14859b8ed4ace5b0e8e97d74eb4
Keoabetswe <keo.nthite@iress.com>
2021-10-21 11:46:29 +0200

    remove unused const navBar

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
 const navSlide = () => {
     const burger = document.querySelector('.burger');
     const nav = document.querySelector('.navbar');
-    const navBar = document.querySelectorAll('.navbar li');
 
     burger.addEventListener('click', () => {
         nav.classList.toggle('nav-active');
